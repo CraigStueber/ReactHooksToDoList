fdf5fd9c4e478f024c8df6e99565b2bb1568233f
Craig A Stueber <58899336+CraigStueber@users.noreply.github.com>
2020-07-11 14:48:30 -0400

    Add files via upload
    Playing with hooks for my upcoming assignment with a local company I hope to be working for very soon

---
+++ precache-manifest.96a5b6805cd544ecb9265dbab89dc2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76dd5029edbeb111ed7f2d6494c0a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f10584bfcb1add4aa878",
+    "url": "/static/css/main.5471d0dc.chunk.css"
+  },
+  {
+    "revision": "17226a663e000cb964cd",
+    "url": "/static/js/2.f3542f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3542f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10584bfcb1add4aa878",
+    "url": "/static/js/main.265069d8.chunk.js"
+  },
+  {
+    "revision": "cdb7b36148d6825f6b21",
+    "url": "/static/js/runtime-main.0f6bb7fd.js"
+  }
+]);
